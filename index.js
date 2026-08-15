79994fb974664edcb2c9d9472b9e96eff1aaf450
behradt1387-creator <behradt1387@gmail.com>
2026-08-15 16:47:57 +0330

    Update TARGET_HOST to new server IP address

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const httpProxy = require('http-proxy');
 
 // آدرس و پورت سرور اصلی خود را مشخص کنید
-const TARGET_HOST = process.env.TARGET_HOST || 'YOUR_SERVER_IP'; // آی‌پی یا دامنه سرور
+const TARGET_HOST = process.env.TARGET_HOST || '84.245.19.88'; // آی‌پی یا دامنه سرور
 const TARGET_PORT = process.env.TARGET_PORT || '8080';            // پورت سرور
 const TARGET_PROTOCOL = process.env.TARGET_PROTOCOL || 'http';   // پروتکل (http یا https)
 
